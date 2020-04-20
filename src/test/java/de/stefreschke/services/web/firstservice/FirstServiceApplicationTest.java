b9e85e13524108eff12ff173bb189a9b366beddf
Stefan Reschke <stefan.reschke@outlook.de>
2020-04-20 23:38:59 +0200

    Configuration in pom.xml

---
+++ src/test/java/de/stefreschke/services/web/firstservice/FirstServiceApplicationTest.java
@@ -0,0 +1,20 @@
+package de.stefreschke.services.web.firstservice;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class FirstServiceApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
