b2154dc6ecbd43790d4e152e9b3c67f2c11ce5fa
Stefan Reschke <stefan.reschke@outlook.de>
2020-11-22 18:41:10 +0100

    Add scm reference to utilize maven release plugin
    [skip ci]prepare release first-service
    
    [skip ci]prepare for next development iteration
    
    Add gitignore
    
    release shell file added
    
    [skip ci]prepare release first-service
    
    More advanced release script
    
    Add SNAPSHOT again
    
    reformat release .sh
    
    [skip ci]prepare release Release 0.0.2
    
    Reformat release .sh again
    
    Add gitignore
    
    [skip ci]prepare release Release-0.0.2
    
    [skip ci]prepare for next development iteration
    
    Remove release prefix for project
    
    [skip ci] prepare release 0.0.3
    
    [skip ci] prepare for next development iteration

---
+++ perform-release.sh
@@ -0,0 +1,1 @@
+./mvnw -B -Dusername=stefreschke -Dpassword=")ns]YQJm0UkVMrsVbyaXXPrUZl[9H{" -DtagNameFormat="@{project.version}" -DscmCommentPrefix="[skip ci] " release:prepare release:perform -Darguments="-Dmaven.deploy.skip=true"
